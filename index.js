76ca918dbdd113accd750b74b45995021aa15f17
Sulaksha Madara <sulakshamadara449@gmail.com>
2025-02-13 06:57:34 +0530

    😈𝐒𝐔𝐋𝐀-𝐌𝐃😈

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, i
 })
 }
 app.get("/", (req, res) => {
-res.send("hey, bot started✅");
+res.send("Hi 𝐒𝐔𝐋𝐀-𝐌𝐃 Bot Started✅");
 });
 app.listen(port, () => console.log(`Server listening on port http://localhost:${port}`));
 setTimeout(() => {
