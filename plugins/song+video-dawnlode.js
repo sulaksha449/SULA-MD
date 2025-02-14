95108d42fd18f2c40fadcf44d955ac7fdfcc58c1
Sulaksha Madara <sulakshamadara449@gmail.com>
2025-02-14 17:51:21 +0530

    😈𝐒𝐔𝐋𝐀-𝐌𝐃😈

--- plugins/song+video-dawnlode.js
+++ plugins/song+video-dawnlode.js
@@ -8,3 +8,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+
+}catch(e){
+console.log(e)
+reply("{$e}")
