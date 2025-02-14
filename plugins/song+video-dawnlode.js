d08e74d8de917bab0126c670e3cea64a2a97bfbc
Sulaksha Madara <sulakshamadara449@gmail.com>
2025-02-14 17:44:03 +0530

    😈𝐒𝐔𝐋𝐀-𝐌𝐃😈

--- plugins/song+video-dawnlode.js
+++ plugins/song+video-dawnlode.js
@@ -1,1 +1,10 @@
+const {cmd , commands} = require('../command')
 
+cmd({
+    pattern: "song",
+    desc: "Download Song",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
